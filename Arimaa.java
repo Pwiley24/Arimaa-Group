de210fae469aed5eaf8f963afef43f1f28441c05
Pwiley24 <97415028+Pwiley24@users.noreply.github.com>
2022-06-09 14:30:38 -0700

    Update Arimaa.java
    must work on checking validation of the game play moves

--- Arimaa.java
+++ Arimaa.java
@@ -21,25 +21,35 @@ public class Arimaa implements ActionListener{
 
 	Container west = new Container();
 	JButton player1 = new JButton("Player 1");
-	JLabel elephantP1 = new JLabel();
-	JLabel camelP1 = new JLabel();
-	JLabel horseP1 = new JLabel();
-	JLabel dogP1 = new JLabel();
-	JLabel catP1 = new JLabel();
-	JLabel rabbitP1 = new JLabel();
+	
+	Container south = new Container();
+	JLabel elephant = new JLabel();
+	JLabel camel = new JLabel();
+	JLabel horse = new JLabel();
+	JLabel horse_2 = new JLabel();
+	JLabel dog = new JLabel();
+	JLabel dog_2 = new JLabel();
+	JLabel cat = new JLabel();
+	JLabel cat_2 = new JLabel();
+	JLabel rabbit = new JLabel();
+	JLabel rabbit_2 = new JLabel();
+	JLabel rabbit_3 = new JLabel();
+	JLabel rabbit_4 = new JLabel();
+	JLabel rabbit_5 = new JLabel();
+	JLabel rabbit_6 = new JLabel();
+	JLabel rabbit_7 = new JLabel();
+	JLabel rabbit_8 = new JLabel();
+	JLabel empty1 = new JLabel();
+	JLabel empty2 = new JLabel();
 	
 	Container east = new Container();
 	JButton player2 = new JButton("Player 2");
-	JLabel elephantP2 = new JLabel();
-	JLabel camelP2 = new JLabel();
-	JLabel horseP2 = new JLabel();
-	JLabel dogP2 = new JLabel();
-	JLabel catP2 = new JLabel();
-	JLabel rabbitP2 = new JLabel();
+
 	
 	int PLAYER_ONE = 1;
 	int PLAYER_TWO = 2;
 	int turn = 0;
+	String playerTurn = null;
 	boolean setUpStatus = false;
 	int piece = 0;
 	int ELEPHANT = 1;
@@ -54,6 +64,17 @@ public class Arimaa implements ActionListener{
 	int dogCount = 0;
 	int catCount = 0;
 	int rabCount = 0;
+	int clicks = 0;
+	int timesThroughSetup = 0;
+	boolean makingMove = false;
+	
+	//Image variables for pieces:
+	ImageIcon elephantImg = new ImageIcon("Images/elephant.png");
+	ImageIcon camelImg = new ImageIcon("Images/camel.png"); 
+	ImageIcon horseImg = new ImageIcon("Images/horse.png"); 
+	ImageIcon dogImg = new ImageIcon("Images/dog.png");
+	ImageIcon catImg = new ImageIcon("Images/cat.png");
+	ImageIcon rabbitImg = new ImageIcon("Images/rabbit.png"); 
 	
 
 	public Arimaa() {
@@ -63,37 +84,31 @@ public class Arimaa implements ActionListener{
 		 * I used this code to resize my Jlabel images to fit better in the JFrame.
 		 */
 		//Image resizing elephant
-		ImageIcon elephantImg = new ImageIcon("Images/elephantImg-removebg-preview.png"); //need to fix this ask
 		Image eImage = elephantImg.getImage();
 		Image newEImage = eImage.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
 		elephantImg = new ImageIcon(newEImage);
 		
 		//Image resizing camel
-		ImageIcon camelImg = new ImageIcon("C:\\Users\\398256\\eclipse\\Arima Project\\camelImg-removebg-preview.png"); //need to fix this ask
 		Image cImage = camelImg.getImage();
 		Image newCImage = cImage.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
 		camelImg = new ImageIcon(newCImage);
 		
 		//Image resizing horse
-		ImageIcon horseImg = new ImageIcon("C:\\Users\\398256\\eclipse\\Arima Project\\horseImg-removebg-preview.png"); //need to fix this ask
 		Image hImage = horseImg.getImage();
 		Image newHImage = hImage.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
 		horseImg = new ImageIcon(newHImage);
 		
 		//Image resizing dog
-		ImageIcon dogImg = new ImageIcon("C:\\Users\\398256\\eclipse\\Arima Project\\dogImg-removebg-preview.png"); //need to fix this ask
 		Image dImage = dogImg.getImage();
 		Image newDImage = dImage.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
 		dogImg = new ImageIcon(newDImage);
 		
 		//Image resizing cat
-		ImageIcon catImg = new ImageIcon("C:\\Users\\398256\\eclipse\\Arima Project\\catImg-removebg-preview"); //need to fix this ask
 		Image catImage = catImg.getImage();
 		Image newCatImage = catImage.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
-		catImg = new ImageIcon(newCImage);
+		catImg = new ImageIcon(newCatImage);
 		
 		//Image resizing rabbit
-		ImageIcon rabbitImg = new ImageIcon("C:\\Users\\398256\\eclipse\\Arima Project\\rabbitImg-removebg-preview.png"); //need to fix this ask
 		Image rImage = rabbitImg.getImage();
 		Image newRImage = rImage.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
 		rabbitImg = new ImageIcon(newRImage);
@@ -108,7 +123,7 @@ public class Arimaa implements ActionListener{
 		center.setLayout(new GridLayout(8,8));
 		for(int y = 0; y < board.length; y++){
 			for(int x = 0; x < board.length; x++){
-				board[y][x] = new JButton(y + ", " + x);
+				board[y][x] = new JButton();
 				if(y == 2 || y == 5){
 					if(x == 2 || x == 5){
 						//set background of button to red
@@ -123,40 +138,55 @@ public class Arimaa implements ActionListener{
 
 		
 		//west layout
-		west.setLayout(new GridLayout(9, 3));
+		west.setLayout(new GridLayout(1,1));
 		west.add(player1);
 		player1.addActionListener(this);
-		elephantP1.setIcon(elephantImg);
-		west.add(elephantP1);
-		camelP1.setIcon(camelImg);
-		west.add(camelP1);
-		horseP1.setIcon(horseImg);
-		west.add(horseP1);
-		dogP1.setIcon(dogImg);
-		west.add(dogP1);
-		catP1.setIcon(catImg);
-		west.add(catP1);
-		rabbitP1.setIcon(rabbitImg);
-		west.add(rabbitP1);
 		panel.add(west, BorderLayout.WEST);
+		
+		//south layout
+		south.setLayout(new GridLayout(2,10));
+		elephant.setIcon(elephantImg);
+		south.add(elephant);
+		camel.setIcon(camelImg);
+		south.add(camel);
+		horse.setIcon(horseImg);
+		south.add(horse);
+		dog.setIcon(dogImg);
+		south.add(dog);
+		cat.setIcon(catImg);
+		south.add(cat);
+		rabbit.setIcon(rabbitImg);
+		south.add(rabbit);
+		rabbit_2.setIcon(rabbitImg);
+		south.add(rabbit_2);
+		rabbit_3.setIcon(rabbitImg);
+		south.add(rabbit_3);
+		rabbit_4.setIcon(rabbitImg);
+		south.add(rabbit_4);
+		
+		south.add(empty1);//next row
+		south.add(empty2);
+		horse_2.setIcon(horseImg);
+		south.add(horse_2);
+		dog_2.setIcon(dogImg);
+		south.add(dog_2);
+		cat_2.setIcon(catImg);
+		south.add(cat_2);
+		rabbit_5.setIcon(rabbitImg);
+		south.add(rabbit_5);
+		rabbit_6.setIcon(rabbitImg);
+		south.add(rabbit_6);
+		rabbit_7.setIcon(rabbitImg);
+		south.add(rabbit_7);
+		rabbit_8.setIcon(rabbitImg);;
+		south.add(rabbit_8);
+		panel.add(south, BorderLayout.SOUTH);
 		
 	
 		//east Layout
-		east.setLayout(new GridLayout(9, 3));
+		east.setLayout(new GridLayout(1,1));
 		east.add(player2);
-		player1.addActionListener(this);
-		elephantP2.setIcon(elephantImg);
-		west.add(elephantP2);
-		camelP2.setIcon(camelImg);
-		west.add(camelP2);
-		horseP2.setIcon(horseImg);
-		west.add(horseP2);
-		dogP2.setIcon(dogImg);
-		west.add(dogP2);
-		catP2.setIcon(catImg);
-		west.add(catP2);
-		rabbitP2.setIcon(rabbitImg);
-		west.add(rabbitP2);
+		player2.addActionListener(this);
 		panel.add(east, BorderLayout.EAST);
 
 		panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -172,8 +202,6 @@ public class Arimaa implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		JButton originalSpot = new JButton();
 		JButton newSpot = new JButton();
-		int clicks = 0;
-		
 		if(e.getSource().equals(player1)) {//if player one is going first
 			turn = PLAYER_ONE;
 			setUp();
@@ -181,29 +209,134 @@ public class Arimaa implements ActionListener{
 			turn = PLAYER_TWO;
 			setUp();
 		}
+		playerTurn = String.valueOf(turn);
+		
 		
 		for(int y = 0; y < board.length; y++) {
 			for(int x = 0; x < board.length; x++) {
+				
 				if(e.getSource().equals(board[y][x])) {//if a board button is clicked
 					if(setUpStatus) {
-						
 						originalSpot = board[y][x];
 						//if it is a valid spot on the board for the setUp, add the image to the button
-						if(checkValid()) {//if a move is valid 
+						if(checkValid(y, originalSpot)) {//if a move is valid
+							if(clicks == 0) {//change background of button to an elephant
+								originalSpot.setIcon(elephantImg);
+								originalSpot.setText(playerTurn + "e");
+								clicks++;
+								JOptionPane.showMessageDialog(panel, "Player, place your camel.");
+							}else if(clicks == 1) {//change button to camel
+								originalSpot.setIcon(camelImg);
+								originalSpot.setText(playerTurn + "c");
+								clicks++;
+								JOptionPane.showMessageDialog(panel, "Player, place your horses.");
+							}else if(clicks == 2 ||
+									clicks == 3) {//change button to horse for two clicks
+								originalSpot.setIcon(horseImg);
+								originalSpot.setText(playerTurn + "h");
+								if(clicks == 3) {
+									JOptionPane.showMessageDialog(panel, "Player, place your dogs.");
+								}
+								clicks++;
+							}else if(clicks == 4 ||
+									clicks == 5) {//change button to dog for two clicks
+								originalSpot.setIcon(dogImg);
+								originalSpot.setText(playerTurn + "d");
+								if(clicks == 5) {
+									JOptionPane.showMessageDialog(panel, "Player, place your cats.");
+								}
+								clicks++;
+							}else if(clicks == 6 ||
+									clicks == 7) {//change button to cat for two clicks
+								originalSpot.setIcon(catImg);
+								originalSpot.setText(playerTurn + "ca");
+								if(clicks == 7) {
+									JOptionPane.showMessageDialog(panel, "Player, place your rabbits.");
+								}
+								clicks++;
+							}else if(clicks == 8 ||
+									clicks == 9 ||
+									clicks == 10 ||
+									clicks == 11 ||
+									clicks ==12 ||
+									clicks == 13 ||
+									clicks == 14 ||
+									clicks == 15) {//change button to rabbit for seven clicks
+								originalSpot.setIcon(rabbitImg);
+								originalSpot.setText(playerTurn + "r");
+								if(clicks == 15) { // if that was the last placement reset clicks for next player or start of game
+									clicks = 0;
+									if(turn == PLAYER_ONE) {
+										turn = PLAYER_TWO;
+									}else {
+										turn = PLAYER_ONE;
+									}
+									timesThroughSetup++;
+									if(timesThroughSetup == 1) {
+										JOptionPane.showMessageDialog(panel, "Next player, place your elephant.");
+									}else if(timesThroughSetup ==2) {
+										setUpStatus = false;
+										if(turn == PLAYER_ONE) {
+											JOptionPane.showMessageDialog(panel, "Time to start the game! Player one make your move!");
+										}else {
+											JOptionPane.showMessageDialog(panel, "Time to start the game! Player two make your move!");
+											
+										}
+									}
+								}else {
+									clicks ++;
+								}
+							}
 							
+							
+						}else {
+							JOptionPane.showMessageDialog(panel, "Place a valid move.");
 						}
-						//change pieces after the one, two, or seven are selected
-					}else {
-						if(clicks % 1 == 0){//if clicks are even (means this click is where player wants to move)
-							//check if this is a valid spot on the board
+						
+					}else {//setup status = false
+						if(clicks == 0){//if first click is made it is selecting the piece to move
+							System.out.println("first click");
+							checkValid(y, originalSpot);
 							//check if pushing or pulling will occur
 							//check if trapping will happen for any pieces
 							//change the background of that button and remove the background of the original spot
 							clicks++;
-						}else {//clicks are odd (means this is the original position, the piece to move
-							//check if this is the players piece
-							//highlight
-							clicks++;
+						}else if(clicks == 1){//if second click is made it is selecting the position to move the original piece to
+							System.out.println("second click");
+							newSpot = board[y][x];
+							makingMove = true;
+							if(checkValid(y, newSpot)) {//if it is a valid move
+								System.out.println("was valid");
+								if(originalSpot.getText().equals("1e") ||
+									originalSpot.getText().equals("2e")) {
+									newSpot.setIcon(elephantImg);
+									newSpot.setText(playerTurn + "e");
+								}else if(originalSpot.getText().equals("1c") ||
+									originalSpot.getText().equals("2c")) {
+									newSpot.setIcon(camelImg);
+									newSpot.setText(playerTurn + "c");
+								}else if(originalSpot.getText().equals("1h") ||
+									originalSpot.getText().equals("2h")) {
+									newSpot.setIcon(horseImg);
+									newSpot.setText(playerTurn + "h");
+								}else if(originalSpot.getText().equals("1d") ||
+									originalSpot.getText().equals("2d")) {
+									newSpot.setIcon(dogImg);
+									newSpot.setText(playerTurn + "d");
+								}else if(originalSpot.getText().equals("1ca") ||
+									originalSpot.getText().equals("2ca")) {
+									newSpot.setIcon(catImg);
+									newSpot.setText(playerTurn + "ca");
+								}else if(originalSpot.getText().equals("1r") ||
+									originalSpot.getText().equals("2r")) {
+									newSpot.setIcon(rabbitImg);
+									newSpot.setText(playerTurn + "r");
+								}
+								originalSpot.setIcon(null);
+								originalSpot.setText("");
+								clicks = 0;
+								makingMove = false;
+							}
 						}
 					}
 				}
@@ -212,16 +345,60 @@ public class Arimaa implements ActionListener{
 		
 	}
 	
-	public boolean checkValid() {
+	
+	
+	public boolean checkValid(int yVal, int xVal, JButton placement, JButton newPlacement) {
 		if (setUpStatus) {//to check if a move is a valid set up move
-			//valid if player one is between x-y
-			//valid if player two is between z-o
-			
+			if(turn == PLAYER_ONE) { //valid player one set up
+				if(yVal == 6 ||
+				   yVal == 7) {
+					if(placement.getIcon() == null) {
+						return true;
+					}
+				}
+			}else {//valid player two set up
+				if(yVal == 0 ||
+					yVal == 1) {
+					if(placement.getIcon() == null) {
+						return true;
+					}
+				}
+			}
 		}else {//to check if a move during the game is valid
-			
+			if(turn == PLAYER_ONE) {
+				if(makingMove == false) {
+					if(placement.getText().equals("1e") ||
+					placement.getText().equals("1c") ||
+					placement.getText().equals("1h") ||
+					placement.getText().equals("1d") ||
+					placement.getText().equals("1ca")||
+					placement.getText().equals("1r")) {//if the button pressed has a friendly piece on it
+						return true;
+					}
+				}else {//you are making a move
+					if(placement.getText().equals("1e") ||
+						placement.getText().equals("1c") ||
+						placement.getText().equals("1h") ||
+						placement.getText().equals("1d") ||
+						placement.getText().equals("1ca")) {//for all pieces that can move backwards
+						
+					}else {//rabbit can't move back
+						if(yVal + 1 == )
+					}
+				}
+			}else if(turn == PLAYER_ONE &&
+				placement.getText().equals("2e") ||
+				placement.getText().equals("2c") ||
+				placement.getText().equals("2h") ||
+				placement.getText().equals("2d") ||
+				placement.getText().equals("2ca")||
+				placement.getText().equals("2r") &&
+				makingMove == false) {//if the button pressed has a friendly piece on it
+				return true;
+			}
+				
 		}
 		return false;
-		
 	}
 	
 	
@@ -232,37 +409,8 @@ public class Arimaa implements ActionListener{
 	 */
 	public void setUp() {
 		setUpStatus = true;
-		
 		//set players pieces up
-		for(int i = 0; i < 2; i++) {//let both player one and two set up their pieces
-			piece = ELEPHANT;
-			JOptionPane.showMessageDialog(panel, "Place your elephant.");
-			if(piece == ELEPHANT && eleCount == 1) {//once one elephant is placed
-				piece = CAMEL;
-				JOptionPane.showMessageDialog(panel, "Place your camel.");
-			}else if(piece == CAMEL && camelCount == 1) {//once one camel is placed
-				piece = HORSE;
-				JOptionPane.showMessageDialog(panel, "Place your 2 horses.");
-			}else if(piece == HORSE && horseCount == 2) {//once two horses are placed
-				piece = DOG;
-				JOptionPane.showMessageDialog(panel, "Place your 2 dogs.");
-			}else if(piece == DOG && dogCount == 2) {//once two dogs are placed
-				piece = CAT;
-				JOptionPane.showMessageDialog(panel, "Place your 2 cats.");
-			}else if(piece == CAT && catCount == 2) {//once two cats are placed
-				piece = RABBIT;
-				JOptionPane.showMessageDialog(panel, "Place your 7 rabbits.");
-			}else if(piece == RABBIT && rabCount == 7) {//once seven rabbits are placed
-				if(turn == PLAYER_ONE) {
-					turn = PLAYER_TWO;
-				}else {
-					turn = PLAYER_ONE;
-				}
-			}
-		}
-		
-		setUpStatus = false;
-			
+		JOptionPane.showMessageDialog(panel, "Player, place your elephant.");
 	}
 
 }
